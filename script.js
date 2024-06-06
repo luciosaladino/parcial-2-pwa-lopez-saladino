e961c67ef82235dc3ce57ab64ef2fce79861e91b
luciosaladino <84475863+luciosaladino@users.noreply.github.com>
2024-06-06 19:02:17 -0300

    trayendo data de la api

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert("probando")
+
+
+const url= "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a"
+
+const info = async () =>{
+
+  const info = await fetch(url)
+  .then(respuesta => respuesta.json())
+  .then(data =>{
+
+    console.log(data)
+
+  })
+}
+
+info();
