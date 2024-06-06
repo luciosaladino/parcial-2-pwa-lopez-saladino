7c3d71965460971f4c3f522601beefd0a57273f5
luciosaladino <84475863+luciosaladino@users.noreply.github.com>
2024-06-06 19:43:01 -0300

    agregando cards, info a las cards y estilos

--- script.js
+++ script.js
@@ -8,8 +8,40 @@ const info = async () =>{
   .then(respuesta => respuesta.json())
   .then(data =>{
 
-    console.log(data)
 
+        let contenedor = document.getElementById("container")
+
+     
+        data.drinks.forEach(trago => {
+
+
+            contenedor.innerHTML +=`  
+            
+            <div class="row">
+            <div class="col s12 m7">
+              <div class="card">
+                <div class="card-image">
+                  <img src="${trago.strDrinkThumb}">
+                  <span class="card-title">${trago.strDrink}</span>
+                </div>
+                <div class="card-content">
+                  <p>${trago.strCategory}</p>
+                </div>
+                <div class="card-action">
+                  <a href="#">Ver más del trago</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          `
+
+         
+
+
+
+            console.log(trago)
+        });
   })
 }
 
