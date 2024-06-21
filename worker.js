9e0be5d7660a21b4c67df3a60e54cc90266da5e6
luciosaladino <84475863+luciosaladino@users.noreply.github.com>
2024-06-21 02:12:48 -0300

    feat: add cache y borrado de caches viejas

--- worker.js
+++ worker.js
@@ -1,13 +1,45 @@
-self.addEventListener("install", e =>{
+let version = "version2"
+
+
+self.addEventListener("install", e => {
 
     console.log("instalando service worker")
+    caches.open(version).then(cache => {
+        cache.addAll([
+            "index.html",
+            "script.js",
+            "css/style.css",
+            "http://127.0.0.1:5500/",
+            "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css",
+            
+            "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"
 
+        ]
+        ).catch(e=>{
+            console.log(e)
+        })
+
+    })
 })
 
 
 
-self.addEventListener("activate", ()=>{
+self.addEventListener("activate", () => {
 
     console.log("service worker activo")
 
+
+   
+    caches.keys().then(
+        key=>{
+            return Promise.all(
+                key.map(cache=>{
+                    if (cache !== version){
+                        console.log("cache eliminado")
+                        return caches.delete(cache)
+                    }
+                })
+            )
+        }
+    )
 })
